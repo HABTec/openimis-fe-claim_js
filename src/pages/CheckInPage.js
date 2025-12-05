dd9436829d60a0e5d2c93ad55d8783b29c9554ba
RihanaE <111240652+RihanaE@users.noreply.github.com>
2025-12-05 14:28:37 +0300

    Remove duplicate import of PublishedComponent

--- src/pages/CheckInPage.js
+++ src/pages/CheckInPage.js
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { 
   PublishedComponent,
-  PublishedComponent,
   historyPush, 
   withModulesManager, 
   withHistory 
